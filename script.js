608dbcefb960c49069661109d90de8ab2a60a9df
rongen <rongen@sg.ibm.com>
2016-09-08 18:11:50 +0800

    change to pokemon

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi! I\'m Smooch Bot!')
+            return bot.say('Hi! I\'m Ryan, your friendly Pokemon catcher!')
                 .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('What\'s your name?'),
+        prompt: (bot) => bot.say('What type of Pokemon would you like to catch?'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
+                .then(() => bot.say(`Great! I'll call you ${name}-hunter
 Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
                 .then(() => 'finish');
         }
